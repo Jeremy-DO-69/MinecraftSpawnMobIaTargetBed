802991b7d75beb705731c1a3025ba56c2cd8eb8a
Jeremy De Oliveira <Jeremy-DO-69>
2025-05-27 17:56:03 +0200

    ia v2

--- src/main/java/com/jdo/CustomMobsSpawnIa/ai/BreakBlocksToBedGoal.java
+++ src/main/java/com/jdo/CustomMobsSpawnIa/ai/BreakBlocksToBedGoal.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BedBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,11 +19,11 @@ import java.util.EnumSet;
 public class BreakBlocksToBedGoal extends Goal {
 
     private BlockPos breakingBlockFront = null;
-    private BlockState stateBlockFront = null;
     private int breakProgressFront = 0;
-    private static final int BREAK_TIME = 40;
+    private int BREAK_TIME = 40;
     private final Mob mob;
     private final BlockPos targetBed;
+    private final BlockPos targetBedOtherHalf;
     private static final Logger LOGGER = LoggerFactory.getLogger("SpawnMobCommand");
     private Player targetPlayer;
 
@@ -35,6 +36,13 @@ public class BreakBlocksToBedGoal extends Goal {
         this.mob = mob;
         this.setFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK));
         this.targetBed = getBedPos(bedId);
+        BREAK_TIME = 60 - (int) (((mob.getBbHeight() % 10) + 1) * 10);
+        LOGGER.info(" Breaktime of {} = {}", mob.getName() , BREAK_TIME);
+        BlockState state = mob.level().getBlockState(targetBed);
+        boolean isHead = state.getValue(BedBlock.PART) == BedPart.HEAD;
+        this.targetBedOtherHalf = isHead
+                ? targetBed.relative(state.getValue(BedBlock.FACING).getOpposite())
+                : targetBed.relative(state.getValue(BedBlock.FACING));
     }
 
    private BlockPos getBedPos(int id) {
@@ -72,10 +80,8 @@ public class BreakBlocksToBedGoal extends Goal {
         }
 
         if (closestBed != null) {
-            LOGGER.info("found closestBed");
             return closestBed;
         } else {
-            LOGGER.info("found basePos");
             return basePos;
         }
     }
@@ -95,8 +101,88 @@ public class BreakBlocksToBedGoal extends Goal {
         }
     }
 
-    @Override
-    public void tick() {
+    public void tickBreakingPath(Mob mob, BlockPos bedPos , BlockPos bedPosOtherHalf) {
+        Level level = mob.level();
+        if (mob.getLastAttacker() instanceof Player player) {
+            if (player.isAlive()) {
+                breakingBlockFront = null;
+                setTargetPlayer(player);
+                mob.setTarget(player);
+            } else {
+                setTargetPlayer(null);
+            }
+        }
+
+        if (targetPlayer == null) {
+            if (breakingBlockFront == null) {
+                mob.getNavigation().moveTo(bedPos.getX(), bedPos.getY(), bedPos.getZ(), 1.0D);
+            } else {
+                mob.getNavigation().stop();
+            }
+
+            if (mob.distanceToSqr(bedPos.getX(), bedPos.getY(), bedPos.getZ()) < 1.5F
+                    || mob.distanceToSqr(bedPosOtherHalf.getX(), bedPosOtherHalf.getY(), bedPosOtherHalf.getZ()) < 1.5F) {
+                if (mob instanceof Creeper) {
+                    mob.level().explode(mob, mob.getX(), mob.getY(), mob.getZ(), 3.0F, Level.ExplosionInteraction.MOB);
+                    mob.discard();
+                } else {
+                    level.destroyBlock(bedPos, true, mob);
+                }
+            }
+
+            if (breakingBlockFront == null) {
+                LOGGER.info("mob height {} {}", mob.getBbHeight(), (int)((mob.getBbHeight() % 10) + 1));
+
+                Vec3 mobCenter = mob.position().add(0, mob.getBbHeight(), 0);
+                Vec3 target = new Vec3(bedPos.getX() + 0.5, bedPos.getY(), bedPos.getZ() + 0.5);
+
+                Vec3 direction = target.subtract(mobCenter).normalize();
+
+                Vec3 frontVec = mob.position().add(direction);
+                int front = 0;
+                int left = 1;
+                int right = 2;
+                for (int x = 1; x <= 3; x++) {
+                    for (int i = 1; i <= (int)((mob.getBbHeight() % 10) + 1); i++) {
+                        int toCheck = 0;
+                        switch (x) {
+                            case 1:
+                                toCheck = front;
+                                break;
+                            case 2:
+                                toCheck = left;
+                                break;
+                            case 3:
+                                toCheck = right;
+                                break;
+
+                        }
+                        BlockPos frontBlockPos = new BlockPos(
+                                (int) Math.floor(frontVec.x),
+                                (int) Math.floor(frontVec.y + i),
+                                (int) Math.floor(frontVec.z + toCheck)
+                        );
+                        BlockState state = level.getBlockState(frontBlockPos);
+
+                        if (!state.isAir() && state.canOcclude()) {
+                            breakingBlockFront = frontBlockPos;
+                            breakProgressFront = 0;
+                            return;
+                        }
+                    }
+                }
+            } else {
+                breakProgressFront++;
+                if (breakProgressFront >= BREAK_TIME) {
+                    mob.level().destroyBlock(breakingBlockFront, true);
+                    breakingBlockFront = null;
+                    breakProgressFront = 0;
+                }
+            }
+        }
+    }
+
+    /*public void oldTickBreakingPath() {
         // Si le joueur ciblé est mort, on revient à l’objectif lit
         if (mob.getLastAttacker() instanceof Player player) {
             if (player.isAlive()) {
@@ -133,6 +219,8 @@ public class BreakBlocksToBedGoal extends Goal {
                             mob.level().destroyBlock(targetBed, true);
                             // 🔄 Détermine l'autre moitié du lit
                             boolean isHead = state.getValue(BedBlock.PART) == BedPart.HEAD;
+
+
                             BlockPos otherHalf = isHead
                                     ? targetBed.relative(state.getValue(BedBlock.FACING).getOpposite())
                                     : targetBed.relative(state.getValue(BedBlock.FACING));
@@ -148,6 +236,9 @@ public class BreakBlocksToBedGoal extends Goal {
             } else {
                 mob.getNavigation().stop();
             }
+
+
+
 
             // Essaye de casser jusqu'à 3 blocs de hauteur devant le mob
             if (breakingBlockFront == null) {
@@ -179,5 +270,11 @@ public class BreakBlocksToBedGoal extends Goal {
                 }
             }
         }
+    }*/
+
+    @Override
+    public void tick() {
+        //oldTickBreakingPath();
+        tickBreakingPath(mob, targetBed, targetBedOtherHalf);
     }
 }
